09a2cfe7c98915ccf13302f929e0513f67df9f31
Mentores Hack <mentores.hack@gmail.com>
2016-07-19 19:58:46 -0430

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('../bower_components/bootstrap/dist/css/bootstrap.css');
+
+import App from "./components/app"
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './app/index.js',
+  output: { 
+    path: 'public',
+    filename: 'bundle.js' 
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test:   /\.css/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test:   /\.(png|gif|jpe?g|svg)$/i,
+        loader: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+};
